396e660ee38325117267f80c6fbbd399cb235b52
MyPC <MyPC@OFALSA>
2022-03-28 18:22:08 +0800

    Second commit with changes

--- Binary Program/src/BinaryProgram.java
+++ Binary Program/src/BinaryProgram.java
@@ -6,9 +6,9 @@ public class BinaryProgram {
 
 		/******************************************************************
 		 * Program 1: Conversion from binary to decimal representation
-		 * Programmer: Write your name here
+		 * Programmer: 
 		 * 
-		 * Class: IT 121        Instructor: 
+		 * Class: IT 121        Instructor: John Mark D. Gabrentina
 		 *        
 		 * Pledge: I have neither given nor received unauthorized aid
 		 * on this program.     (signature on file)
@@ -33,36 +33,6 @@ public class BinaryProgram {
 		      	// Declaration of variable to hold the current bit
 		         int bit;
 		      
-		      	
-		       	//////////////////////////////////////
-		      	// ADD YOUR CODE HERE
-		      	// FOLLOW THE EXAMPLE PROVIDED
-		      	// FOR THE LAST BIT, THE 6th
-		      	//////////////////////////////////////
-		/*      				
-		         bit=binary%10;//get the last bit (6th)
-		         decimal=decimal+bit*1;//add it, multiplied by the corresponding power of 2
-		         binary=binary/10;//get rid of the 6th bit, now the 5th bit is last
-		      	
-		         bit=binary%10;//get the last bit (5th)
-		         decimal=decimal+bit*2;//add it, multiplied by the corresponding power of 2
-		         binary=binary/10;//get rid of the 5th bit, now the 4th bit is last
-		      	
-		         bit=binary%10;//get the last bit (4th)
-		         decimal=decimal+bit*4;//add it, multiplied by the corresponding power of 2
-		         binary=binary/10;//get rid of the 4th bit, now the 3rd bit is last
-		      	
-		         bit=binary%10;//get the last bit (3rd)
-		         decimal=decimal+bit*8;//add it, multiplied by the corresponding power of 2
-		         binary=binary/10;//get rid of the 3rd bit, now the 2nd bit is last
-		      	
-		         bit=binary%10;//get the last bit (2nd)
-		         decimal=decimal+bit*16;//add it, multiplied by the corresponding power of 2
-		         binary=binary/10;//get rid of the 2nd bit, now the 1st bit is last 
-		      	
-		         bit=binary%10;//get the last bit (1st)
-		         decimal=decimal+bit*32;//add it, multiplied by the corresponding power of 2
-		*/
 
 			int i=0;
 			int power2=1;
